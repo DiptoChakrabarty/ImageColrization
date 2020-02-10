09ff54341e97bd7bd19f8070c684dc4dedc32f7d
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-10 22:43:47 +0530

    encoder model

--- neural.py
+++ neural.py
@@ -2,7 +2,7 @@ import keras
 import cv2
 import os
 import numpy as np
-from keras.models import Sequential
+from keras.models import Sequential,Model
 from keras.layers import Dense,Conv2D,Flatten,MaxPooling2D,Input
 
 
@@ -12,17 +12,23 @@ op="./black"
 
 itl=[os.path.join(ip,f) for f in os.listdir(ip)]
 otl=[os.path.join(op,f) for f in os.listdir(op)]
-
+latent_dim=256
 #create encoder
-model=Input(shape=(400,350,1),name="encoder_input")
-model.add(Conv2D(64,kernel_size=3,activation='relu',input_shape=(400,350,1)))
-model.add(MaxPooling2D(pool_size=(2,2)))
+t=Input(shape=(400,350,1),name="encoder_input")
+model=t
+#Allows us to create model only knowing the inputs 
+model=Conv2D(64,kernel_size=3,activation='relu',strides=2)(model)
+model=Conv2D(128,kernel_size=3,activation='relu',strides=2)(model)
+model=Conv2D(256,kernel_size=3,activation='relu',strides=2)(model)
 
-model.add(Conv2D(32,kernel_size=3,activation='relu'))
-model.add(Flatten())
-model.add(Dense(10,activation="relu"))
 #Generate Latent Layer
-x=Flatten()(model)
-model.summary()
+model=Flatten()(model)
+latent=Dense(latent_dim,name="latent_vector")(model)
+
+# make encoder model
+encoder=Model(t,latent,name="encoder_layers")
+encoder.summary()
+
+
 
 
