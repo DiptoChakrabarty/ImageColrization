868f7f4259f43b6734ce5135431996e4ac71af48
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-10 22:30:03 +0530

    encoder function

--- neural.py
+++ neural.py
@@ -3,7 +3,7 @@ import cv2
 import os
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense,Conv2D,Flatten,MaxPooling2D
+from keras.layers import Dense,Conv2D,Flatten,MaxPooling2D,Input
 
 
 
@@ -14,12 +14,15 @@ itl=[os.path.join(ip,f) for f in os.listdir(ip)]
 otl=[os.path.join(op,f) for f in os.listdir(op)]
 
 #create encoder
-model=Sequential()
+model=Input(shape=(400,350,1),name="encoder_input")
 model.add(Conv2D(64,kernel_size=3,activation='relu',input_shape=(400,350,1)))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Conv2D(32,kernel_size=3,activation='relu'))
 model.add(Flatten())
 model.add(Dense(10,activation="relu"))
-model.compile(loss="mse",optimizer="adam")
+#Generate Latent Layer
+x=Flatten()(model)
 model.summary()
+
+
