3a62138f42cc3406cc1e5489b48a7dbb8545dc67
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-16 22:24:53 +0530

    functional decoder model

--- neural.py
+++ neural.py
@@ -16,7 +16,7 @@ latent_dim=256
 t=Input(shape=(400,350,1),name="encoder_input")
 model=t
 #Allows us to create model only knowing the inputs 
-'''model=Conv2D(64,kernel_size=3,activation='relu',strides=2)(model)
+model=Conv2D(64,kernel_size=3,activation='relu',strides=2)(model)
 model=Conv2D(128,kernel_size=3,activation='relu',strides=2)(model)
 model=Conv2D(256,kernel_size=3,activation='relu',strides=2)(model)
 
@@ -26,11 +26,11 @@ latent=Dense(latent_dim,name="latent_vector")(model)
 
 # make encoder model
 encoder=Model(t,latent,name="encoder_layers")
-encoder.summary()'''
+encoder.summary()
 
 #encoder model
 
-encoder=Sequential()
+'''encoder=Sequential()
 encoder.add(Conv2D(64,kernel_size=3,activation='relu',input_shape=(400,350,1),strides=2))
 encoder.add(Conv2D(128,kernel_size=3,activation='relu',strides=2))
 encoder.add(Conv2D(256,kernel_size=3,activation='relu',strides=2))
@@ -45,17 +45,32 @@ encoder.summary()
 # Decoder Model
 decoder=Sequential()
 decoder.add(Dense(400*350*1))
-decoder.Reshape((400,350,1))
+decoder.add(Reshape((400,350,1)))
 decoder.add(Conv2DTranspose(256,kernel_size=3,activation='relu',padding='same'))
 decoder.add(Conv2DTranspose(128,kernel_size=3,activation='relu',padding='same'))
 decoder.add(Conv2DTranspose(64,kernel_size=3,activation='relu',padding='same'))
-decoder.add(Conv2DTranspose(kernel_size=3,
+decoder.add(Conv2DTranspose(itl[0].shape[-1],kernel_size=3,
                           activation='sigmoid',
                           padding='same',
                           name='decoder_output'))
 
+decoder.summary()
+pred=encoder.predict'''
 
 
+# Decoder model
+latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
+dec = Dense(400*350*1)(latent_inputs)
+dec = Reshape((400,350,1))(x)
+
+dec = Conv2DTranspose(256,kernel_size=3,activation="relu",padding="same",strides=2)(dec)
+dec = Conv2DTranspose(128,kernel_size=3,activation="relu",padding="same",strides=2)(dec)
+dec = Conv2DTranspose(64,kernel_size=3,activation="relu",padding="same",strides=2)(dec)
+
+output = Conv2DTranspose(filters=1,kernel_size=3,activation="sigmoid",padding="same",name="decoder_layers")(dec)
+
+decoder = Model(latent_inputs,output,name="decoder_model")
+decoder.summary()
 
 
 
